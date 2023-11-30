76222dac3a03d54218dc3e3400a41d3dccf4d733
Elias Lehman <77815553+eliaslehman@users.noreply.github.com>
2023-11-30 11:26:57 -0800

    added build files

---
+++ build/lib/grblc/convert/__init__.py
@@ -0,0 +1,6 @@
+""" TO USE:
+Import this file into a directory to be used in your command line interfance (e.g., terminal on mac, cmd.exe on windows)
+Make sure you are in the directory, and then start Jupyter notebook or Jupyter lab / any other Python instance from there
+and import as any other module.
+"""
+from .convert import *
---
+++ build/lib/grblc/fitting_LATEST/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Sam Young, Nicole Osborn"
+
+from ..util import get_dir, set_dir
+from .model import *
+from .lightcurve import *
+from .outlier import *
